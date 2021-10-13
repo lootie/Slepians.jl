4d4576ba95000a16e5c042788d90c6bbef93a7a8
Charlotte Haley <haleycharlotte@gmail.com>
2021-10-13 16:32:27 -0500

    testfile committed

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -7,15 +7,39 @@ K = 7
 NW = 4.0
 W = 0.08
 
-t = sort(randn(1024))
+ev, v = dpss_tapers(N, W, K, :both)
 
+@testset "dpss test" begin
+  
+  # Eigenvalues
+  @test ev[1:3] ≈ [0.031031839127290427, 0.03103311455746982, 0.031034387523034522]
+
+  # Eigenvectors
+  @test v[1:3] ≈[0.1588842745838488, 0.0011144647659785548, 1.2600650734933855e-6]
+
+end
+
+
+t = sort(randn(1024))
 ev, v = gpss_orth(W, K, t, 0.1)
 
 @testset "gpss test" begin 
   
-  # Expected jackknife variance
-  @test ev[1] ≈ [0.8488638017599491, 0.4569294729712821, 0.2890809140379784]
+  # Eigenvalues
+  @test ev[1:3] ≈ [0.8488638017599491, 0.4569294729712821, 0.2890809140379784]
 
-  @test v[1:3] ≈ [0.13416225814078597, 0.09152484752991513, 0.1582872094843311]
+  # Eigenvectors
+  @test v[1:3] ≈ [0.14268716855395974, 0.09159710145322413, 0.1682672984982918]
+
+end
+
+sleps = customsleps(5, 10.0, (16, 8), exact = true)
+
+@testset "2D Spherical Slepians" begin
+
+  @test sleps[1][1:3] ≈ Complex{Float64}[1.4165175331754862 + 0.0im, 1.302170654345073 + 0.0im, 1.1413498706076248 + 0.0im]
+
+  # Exact 2D Slepians
+  @test sleps[2][2][1:3] ≈ Complex{Float64}[-0.10725724727346368 + 0.0im, -0.17892535707531507 + 0.0im, -0.23728562389432664 + 0.0im]
 
 end
