19e90d078d884dfcb73ed2e4f568fcd83169cf9f
Charlotte Haley <haleycharlotte@gmail.com>
2021-10-13 16:20:40 -0500

    test set Random dep added

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -1,11 +1,21 @@
 
+using Random
+Random.seed!(123)
+
 N = 1024
 K = 7
 NW = 4.0
+W = 0.08
 
-@testset "dpss test" begin 
+t = sort(randn(1024))
+
+ev, v = gpss_orth(W, K, t, 0.1)
+
+@testset "gpss test" begin 
   
   # Expected jackknife variance
-  @test sin(pi/2) ≈ 1.0 
+  @test ev[1] ≈ [0.8488638017599491, 0.4569294729712821, 0.2890809140379784]
+
+  @test v[1:3] ≈ [0.13416225814078597, 0.09152484752991513, 0.1582872094843311]
 
 end
